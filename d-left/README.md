26898ced3a3658195fc6d871f7f5c93161e0149d
mengxiang0811 <qiaobin66@163.com>
2016-02-22 09:15:30 -0500

    add d-left implemented by Rishi

---
+++ d-left/README.md
@@ -0,0 +1,15 @@
+# d-left
+
+### Run
+```
+gcc -o murmur3 murmur3.c
+gcc -o hash hash.c murmur3.o
+./hash
+```
+
+### Dependency
+[Murmur3 hashing](https://github.com/PeterScott/murmur3)
+
+### Refereces
+1. [Algorithms - ESA 2006](https://books.google.co.in/books?id=eKOoCAAAQBAJ&pg=PA688&lpg=PA688&dq=d-left+deletion+hashing&source=bl&ots=_t4AgfeEqs&sig=6pc27jDRL5SybempvGOaUr62jlE&hl=en&sa=X&redir_esc=y#v=onepage&q=d-left%20hashing&f=false)
+2. [Slide](http://www.arl.wustl.edu/~jst/cse/577/lec/exactMatch.pdf)
---
+++ d-left/hash.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "murmur3.h"
+
+
+const int BUCKET_HEIGHT = 2;    // ### need to change this
+const int SUBTABLE_SIZE = 5;
+const int TABLE_SIZE = 13;
+const uint32_t SEED = 42;
+
+
+struct Bucket
+{
+	int counter;              // keep count of filled cells
+	uint32_t fingerprint[BUCKET_HEIGHT];
+};
+
+
+struct SubTable
+{
+	struct Bucket buckets[SUBTABLE_SIZE];   /* bucket array*/
+};
+
+
+struct Table
+{
+	struct SubTable subtables[TABLE_SIZE];      /* subtables array, bucket count, subtables count */
+};
+
+
+int * getTargets(uint32_t key, struct Table *td)
+{
+	static int bucketID[TABLE_SIZE];
+	uint32_t out;
+	MurmurHash3_x86_32((const void *)&key, sizeof(uint32_t), SEED, &out);					//calculate the hash value for each subtables,
+	int position = out % SUBTABLE_SIZE;
+	for (int i = 0; i < TABLE_SIZE; i++) {
+		if (td->subtables[i].buckets[out % SUBTABLE_SIZE].counter == BUCKET_HEIGHT) {
+			bucketID[i] = -1;
+			continue;
+		}
+		bucketID[i] = position;   		// Then divide hash value by subtable size
+	}
+	return bucketID;
+}
+
+
+//Check if no room in any bucket   
+bool checkEmptyArray(int *bucketList)
+{
+	bool emptyArr = false;
+	for (int i = 0; i < TABLE_SIZE; i++) {
+		if (bucketList[i] != -1) {
+			emptyArr = true;
+			break;
+		}
+	}
+	return emptyArr;
+}
+
+
+bool inserting(uint32_t key, struct Table *td)
+{
+	int *bucketList = getTargets(key, td);
+	bool emptyArr = checkEmptyArray(bucketList);
+	if (emptyArr) {
+		int minCount = BUCKET_HEIGHT, index = 0;
+		for (int i = 0; i < TABLE_SIZE; i++) {
+			if (bucketList[i] != -1) {
+				if (minCount > td->subtables[i].buckets[bucketList[i]].counter) {
+					minCount = td->subtables[i].buckets[bucketList[i]].counter;
+					index = i;
+				}
+			}
+		}
+		td->subtables[index].buckets[bucketList[index]].fingerprint[minCount] = key;
+		td->subtables[index].buckets[bucketList[index]].counter += 1;
+	} else {
+		printf("[Error]:Key already exists\n");
+		return false;
+	}
+	return true;
+}
+
+
+int * getBuckets(uint32_t key, struct Table *td)
+{
+	static int bucketID[TABLE_SIZE];
+	uint32_t out;
+	MurmurHash3_x86_32((const void *)&key, sizeof(uint32_t), SEED, &out);					//calculate the hash value for each subtables,
+	for (int i = 0; i < TABLE_SIZE; i++) {
+		if (td->subtables[i].buckets[out % SUBTABLE_SIZE].counter == 0) {
+			bucketID[i] = -1;
+			continue;
+		}
+		bucketID[i] = out % SUBTABLE_SIZE;   		// Then divide hash value by subtable size
+	}
+	return bucketID;
+}
+
+
+bool lookup(uint32_t key, struct Table *td)
+{
+	int *bucketList = getBuckets(key, td);
+	bool emptyArr = checkEmptyArray(bucketList);
+	if (emptyArr) {
+		for (int i = 0; i < TABLE_SIZE; i++) {
+			if (bucketList[i] != -1) {
+				for (int j = 0; j < BUCKET_HEIGHT; j++) {
+					if (td->subtables[i].buckets[bucketList[i]].fingerprint[j] == key) {
+						return true;
+					}
+				}
+			}
+		}
+	} else {
+		printf("[Error]: Key does not exists\n");
+		return false;
+	}
+	return false;
+}
+
+
+bool delete(uint32_t key, struct Table *td)
+{
+	int *bucketList = getBuckets(key, td);
+	bool emptyArr = checkEmptyArray(bucketList);
+	bool deleted = false;
+	if (emptyArr) {
+		int bucketIndex, cellIndex;
+		for (int i = 0; i < TABLE_SIZE; i++) {
+			if (bucketList[i] != -1) {
+				for (int j = 0; j < BUCKET_HEIGHT; j++) {
+					if (td->subtables[i].buckets[bucketList[i]].fingerprint[j] == key) {
+						bucketIndex = i;
+						cellIndex = j;
+						deleted = true;
+						break;
+					}
+				}
+			}
+		}
+		if (deleted) {
+			for (int i = cellIndex; i < BUCKET_HEIGHT; i++) {
+				td->subtables[bucketIndex].buckets[bucketList[bucketIndex]].fingerprint[i] = td->subtables[bucketIndex].buckets[bucketList[bucketIndex]].fingerprint[i + 1];
+			}
+			td->subtables[bucketIndex].buckets[bucketList[bucketIndex]].counter -= 1;
+			printf("The key - %d is deleted successfully\n ", key);
+			return true;
+		} else {
+			printf("[Error]: Key does not exists\n");
+			return false;
+		}
+	} else {
+		printf("[Error]: Key does not exists\n");
+		return false;
+	}
+	return false;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	struct Table T1;
+	uint32_t key = 50;
+	bool output = inserting(key, &T1);
+	output = lookup(key, &T1);
+	output = delete(key, &T1);
+	printf("%d\n", output);
+	return 0;
+}
